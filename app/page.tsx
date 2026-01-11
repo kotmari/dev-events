bd613b1fbd90f65a5ba2c332f5a46935d861f11e
kotmari <mariannakot79@gmail.com>
2026-01-11 23:35:13 +0200

    implement api routes, server actions and caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,37 @@
-import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import { events } from "@/lib/constants";
+import EventCard from "@/components/EventCard";
+import {IEvent} from "@/database";
+import { cacheLife } from "next/cache";
 
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
+const Page = async () => {
+    "use cache"
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
-export default function Home() {
-  return (
-    <section>
-      <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&rsquo;t Miss</h1>
-      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
-      <ExploreBtn />
-      <div className="mt-20 space-y-7">
-        <h3>Featured Events</h3>
-        <ul className="events">
-          {events.map((event) => (
-            <li key={event.title} className="list-none">
-              {/* <EventCard title={event.title} image={event.image} /> by */}
-              <EventCard {...event} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </section>
-  );
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&rsquo;t Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+            <ExploreBtn />
+
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
 }
+
+export default Page;
